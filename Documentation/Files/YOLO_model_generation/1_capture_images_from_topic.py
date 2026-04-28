2cd4612fbd7ba4fa0afb30eb0d89c257310196ee
flaviaserret04 <flserrem7@alumnes.ub.edu>
2026-04-28 18:28:36 +0200

    Imatges YOLO

--- Documentation/Files/YOLO_model_generation/1_capture_images_from_topic.py
+++ Documentation/Files/YOLO_model_generation/1_capture_images_from_topic.py
@@ -12,12 +12,12 @@ from cv_bridge import CvBridge
 # GLOBAL PARAMETERS
 # ==============================
 IMAGE_TOPIC = "/image_raw"
-INTERVAL_SECONDS = 0.5
+INTERVAL_SECONDS = 0.1
 FILENAME_PREFIX = "image"
 START_INDEX = 1
 
 # Relative to the project root directory 
-OUTPUT_RELATIVE_PATH = "photos/Forbidden"
+OUTPUT_RELATIVE_PATH = "photos/Nothing"
 
 
 class ImageCaptureNode(Node):
